c6ee16459a7887ec9e8f980783b9cd33ddf3f9eb
Grzegorz Pająk <gregory353a@gmail.com>
2017-10-10 23:23:40 +0200

    prepare timeOfWork

--- main.cpp
+++ main.cpp
@@ -5,24 +5,36 @@
 
 using namespace std::chrono;
 
-void fun1(std::promise<int> px){
-    int y=5;
-    try {
-        px.set_value(y);
-    } catch(...){
-        px.set_exception(std::current_exception());
-    }
-}
-
-void fun2(std::future<int> fx){
-    std::cout<< "Czekam" << std::endl;
-    try{
-        int x = fx.get();
-        std::cout << "Otrzymałem: " << x << std::endl;
-    } catch(...){
-        std::cout << "Błąd" << std::endl;
-    }
-}
+//void fun1(std::promise<int> px){
+//    int y=5;
+//    try {
+//        px.set_value(y);
+//    } catch(...){
+//        px.set_exception(std::current_exception());
+//    }
+//}
+//
+//void fun2(std::future<int> fx){
+//    std::cout<< "Czekam" << std::endl;
+//    try{
+//        int x = fx.get();
+//        std::cout << "Otrzymałem: " << x << std::endl;
+//    } catch(...){
+//        std::cout << "Błąd" << std::endl;
+//    }
+//}
+//
+//void fun3(){
+//    std::promise<int> px;
+//    std::future<int> fx = px.get_future();
+//    auto time0 = high_resolution_clock::now();
+//    std::thread t1{fun1, std::move(px)};
+//    std::thread t2{fun2, std::move(fx)};
+//    t1.join();
+//    t2.join();
+//    auto time1 = high_resolution_clock::now();
+//    std::cout << duration_cast<seconds>(time1-time0).count() << "s" << std::endl;
+//}
 
 void workTime(int duration = 8){
     std::this_thread::sleep_for(std::chrono::seconds(duration*60*60-15*60));
@@ -41,40 +53,42 @@ void lunchBreak(int duration = 30){
 }
 
 void breakClock() {
-    while(TRUE){
-        std::this_thread::sleep_for(std::chrono::seconds(60*60));
+    while (TRUE) {
+        std::this_thread::sleep_for(std::chrono::seconds(60 * 60));
         {
             std::cout << "Break \a" << std::endl;
             MessageBox(nullptr, "Czas na przerwe!", "Uwaga, uwaga!!!", MB_OK);
         }
         auto breakTimeStart = high_resolution_clock::now();
         MessageBox(nullptr, "Przerwa", "Przerwa", MB_OK);
-        auto breakDuration =  duration_cast<minutes>(high_resolution_clock::now()-breakTimeStart).count();
+        auto breakDuration = duration_cast<minutes>(high_resolution_clock::now() - breakTimeStart).count();
         std::string durationNotify = "Przerwa trwała: " + breakDuration;
         durationNotify += " minut";
         MessageBox(nullptr, durationNotify.c_str(), "Długość przerwy", MB_OK);
-    //todo zapis do pliku
+        //todo zapis do pliku
     }
+}
+void timeOfWork(){
+    auto startTimeStamp = high_resolution_clock::now();
+    if(/*stopsignal*/){
+        auto stopTimeStamp = high_resolution_clock::now();
+    }
+
+
 }
 
 int main() {
+    std::thread timeCounterThread{timeOfWork};
     std::cout << "Hello, World!" << std::endl;
-    std::promise<int> px;
-    std::future<int> fx = px.get_future();
     HWND okno = GetConsoleWindow();
-   ShowWindow( okno, SW_HIDE );
+    ShowWindow( okno, SW_HIDE );
 
     std::thread breakThread{breakClock};
 
     breakThread.join();
-//    auto time0 = high_resolution_clock::now();
-//    std::thread t1{fun1, std::move(px)};
-//    std::thread t2{fun2, std::move(fx)};
-//    t1.join();
-//    t2.join();
+
+
     ShowWindow( okno, SW_SHOW );
-//    auto time1 = high_resolution_clock::now();
-//    std::cout << duration_cast<seconds>(time1-time0).count() << "s" << std::endl;
     std::cin.get();
     std::cin.get();
     return 0;
